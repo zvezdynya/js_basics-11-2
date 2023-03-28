b1bf65838e9654db3c569d418724fc4dd11a34a2
zvezdynya <alya154@rambler.ru>
2023-03-28 22:32:10 +0300

    add comments by function

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,12 +1,10 @@
-const firstPrices = document.querySelectorAll('.price');
-const discountPrices = document.querySelectorAll('.discount_price');
-const weigths = document.querySelectorAll('.weight');
-console.log(weigths);
-
-const allProducts = document.querySelector('.all_products');
-const allDiscount = document.querySelector('.all_discount');
-const total = document.querySelector('.total');
-const btnDiscount = document.querySelector('#discount');
+const firstPrices = document.querySelectorAll('.price'); //получаем все цены
+const discountPrices = document.querySelectorAll('.discount_price'); //получаем инпуты для цен после дисконта
+const weigths = document.querySelectorAll('.weight'); //получаем вес каждого товара
+const allProducts = document.querySelector('.all_products'); //вывод общего количества товаров
+const allDiscount = document.querySelector('.all_discount'); //вывод общей скидки
+const total = document.querySelector('.total'); //общая стоимость корзины
+const btnDiscount = document.querySelector('#discount'); //кнопка купона на скидку
 const currency = ' руб.';
 const weightCurrency = ' кг.';
 
@@ -16,7 +14,7 @@ const backetWeight = () => {
         weight += +el.textContent;
     });
     return weight.toFixed(2);
-};
+}; //высчитываем суммарный вес корзины
 
 let sum = 0;
 let discountSum = 0;
@@ -28,46 +26,46 @@ const discountTotal = () => {
         discountSum += + discountPrices[i].value;
     }
     return discountSum;
-};
+}; //считаем общую сумму скидки и добавляем новую стоимость для товаров
 
 const calcTotal = () => {
     firstPrices.forEach(el => {
         sum += +el.value;
     });
     return sum;
-};
+}; //считаем общую стоимость товаров до скидки
 
 function outTotal() {
     sum = 0;
     weight = 0;
     allProducts.innerHTML = '';
     return allProducts.innerHTML = `Всего товаров(${firstPrices.length}/${backetWeight() + weightCurrency}): <span>${calcTotal()}${currency}</span>`;
-}
+} //выводим общее кол-во товаров, вес и стоимость
 
 function outDiscount() {
     sum = 0;
     discountSum = 0;
     allDiscount.innerHTML = '';
     return allDiscount.innerHTML = `Скидка: <span>${calcTotal() - discountTotal()}${currency}</span>`;
-}
+} //выводим общую скидку
 
 function outDiscountTotal() {
     discountSum = 0;
     total.innerHTML = '';
     return total.innerHTML = `Итоговая стоимость: <span>${discountTotal()}${currency}</span>`;
-}
+} //выводим общую стоимость с учетом скидки
 
 function outAllData () {
     outTotal();
     outDiscount();
     outDiscountTotal();
-}
+} //функция-обертка для всех функций
 
 document.addEventListener('DOMContentLoaded', function() {
     outTotal();
-});
+}); //событие вывода количества товаров, веса и стоимости без скидки в момент загрузки страницы
 
-btnDiscount.addEventListener('click', outAllData, {once: true});
+btnDiscount.addEventListener('click', outAllData, {once: true}); //одноразовое срабатывание купона
 
 //del elem
 
@@ -80,4 +78,4 @@ buttonsDelete.forEach(btn => {
         const targetParent = target.closest('article');
         targetParent.remove();
     });
-});
+}); //удаление товара из корзины
